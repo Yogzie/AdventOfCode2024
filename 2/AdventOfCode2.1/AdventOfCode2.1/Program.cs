3cf7a547d8b4e559387377bf025ed8258ed86b02
Yogett HD <14hodgettd@gmail.com>
2024-12-08 20:17:05 +0000

    Finished Part 1

--- 2/AdventOfCode2.1/AdventOfCode2.1/Program.cs
+++ 2/AdventOfCode2.1/AdventOfCode2.1/Program.cs
@@ -2,8 +2,47 @@
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        var input = File.ReadLines($"{Environment.CurrentDirectory}\\input.txt");
+        int total = 0;
+        
+        foreach (var line in input)
+        {
+            bool increasing = false;
+            bool safe = true;
+            var report = line.Split(" ");
+            for (var i = 0; i < report.Length - 1; i++)
+            {
+                if (i == report.Length - 1) { break; }
+                int currentNum = Convert.ToInt32(report[i]);
+                int nextNum = Convert.ToInt32(report[i + 1]);
+                if (i == 0 && currentNum > nextNum)
+                {
+                    increasing = false;
+                }
+                else if ((i == 0) && currentNum < nextNum)
+                {
+                    increasing = true;
+                }
+                
+                if (increasing && currentNum >= nextNum) { safe = false; break; }
+                if (!increasing && currentNum <= nextNum) { safe = false; break; }
+                
+                // We've covered the increase / decrease pattern.
+                if (!(Math.Abs(currentNum - nextNum) <= 3)) { safe = false; break; }
+                
+                if (currentNum == nextNum) { safe = false; break; }
+
+            }
+
+            if (safe)
+            {
+                //Console.WriteLine(line);
+                total++;
+            }
+        }
+        Console.WriteLine(total);
+        
     }
 }
--- 2/AdventOfCode2.1/AdventOfCode2.1/obj/Debug/net9.0/AdventOfCode2.1.AssemblyInfo.cs
+++ 2/AdventOfCode2.1/AdventOfCode2.1/obj/Debug/net9.0/AdventOfCode2.1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCode2.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef6c8f70bf53b7c0c366e12f908de0b72a2eeeac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c12d5c7983665c18797c6947c6253113fc38c8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCode2.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCode2.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

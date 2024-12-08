541293e9dc8e2ddb581d7378131f8df32aeeae68
Yogett HD <14hodgettd@gmail.com>
2024-12-08 00:36:11 +0000

    Completed part 2

--- 1/AdventOfCode1.1/AdventOfCode1.1/Program.cs
+++ 1/AdventOfCode1.1/AdventOfCode1.1/Program.cs
@@ -1,5 +1,11 @@
 ﻿namespace AdventOfCode1._1;
 
+class QuantityOfNumber
+{
+    public int Number { get; set; }
+    public int Quantity { get; set; }
+}
+
 class Program
 {
     static void Main()
@@ -8,27 +14,68 @@ class Program
         var leftList = new int[input.Length];
         var rightList = new int[input.Length];
         var total = 0;
+        var part2Total = 0;
         // We get two sets of numbers, we need to split them into their own lists and sort them in size order.
+        
+        var leftOccurences = new List<QuantityOfNumber>();
+        var rightOccurences = new List<QuantityOfNumber>();
         
         // Building the individual lists.
         for (var i = 0; i < input.Length; i++)
         {
             var splitInput = input[i].Split("   ");
             leftList[i] = int.Parse(splitInput[0]);
+            AddOccurence(leftList[i], ref leftOccurences);
             rightList[i] = int.Parse(splitInput[1]);
+            AddOccurence(rightList[i], ref rightOccurences);
+        }
+        // Not needed for part 2
+        //leftList = (leftList.OrderBy(x => x)).ToArray();
+        //rightList = (rightList.OrderBy(x => x)).ToArray();
+
+        
+        // Part 2 Processing:
+
+        foreach (var m in leftOccurences)
+        {
+            // We have our occurence, 3 : 3
+            
+            foreach (var y in rightOccurences)
+            {
+                // Match the number in rightOccurrences
+                if (m.Number != y.Number) { continue; }
+
+                part2Total += ((Math.Min(y.Quantity, m.Quantity) * y.Number) * (y.Quantity));
+                Console.WriteLine($"Part 2: {part2Total}");
+            }
         }
         
-        leftList = (leftList.OrderBy(x => x)).ToArray();
-        rightList = (rightList.OrderBy(x => x)).ToArray();
-
+        
+        
+        
+        // Not needed for part 2
         for (var b = 0; b < input.Length; b++)
         {
-            Console.WriteLine($"Left List: {leftList[b]} Right List: {rightList[b]}");
+            //Console.WriteLine($"Left List: {leftList[b]} Right List: {rightList[b]}");
             
             var difference = Math.Abs(leftList[b] - rightList[b]); 
             total += difference;
-            Console.WriteLine($"Difference: {difference}");
+            //Console.WriteLine($"Difference: {difference}");
         }
-        Console.WriteLine($"Total Difference: {total}");
+        //Console.WriteLine($"Total Difference: {total}");
+    }
+    public static void AddOccurence(int num, ref List<QuantityOfNumber>list)
+    {
+        foreach (var a in list)
+        {
+            if (a.Number == num)
+            {
+                a.Quantity++;
+                return;
+            }
+        }
+        
+        list.Add(new QuantityOfNumber { Number = num, Quantity = 1 });
     }
 }
+
--- 1/AdventOfCode1.1/AdventOfCode1.1/obj/Debug/net9.0/AdventOfCode1.1.AssemblyInfo.cs
+++ 1/AdventOfCode1.1/AdventOfCode1.1/obj/Debug/net9.0/AdventOfCode1.1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCode1.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beaf0d0beca690e0b32b4458d02298d017db2ce5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8276eb572c3eda0a7b1b0c9b08155c177f70d06")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCode1.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCode1.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

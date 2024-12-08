0316dcf31555d3d87b51f7fcd182f0f3f7ee13f3
Yogett HD <14hodgettd@gmail.com>
2024-12-08 00:06:47 +0000

    Part 1 of Day 1 complete

--- 1/AdventOfCode1.1/AdventOfCode1.1/obj/Debug/net9.0/AdventOfCode1.1.AssemblyInfo.cs
+++ 1/AdventOfCode1.1/AdventOfCode1.1/obj/Debug/net9.0/AdventOfCode1.1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventOfCode1.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58f01203f4e5bed83f00678d8500518d3d5a6e73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beaf0d0beca690e0b32b4458d02298d017db2ce5")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventOfCode1.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventOfCode1.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
